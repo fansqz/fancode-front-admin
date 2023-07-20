29703de3a1d5c09f2b3be5bc9f4cc33dfbd1ae2f
fzw-code <81961566+fzw-code@users.noreply.github.com>
2023-07-20 17:07:58 +0800

    add stylelint

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,10 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import { createApp } from 'vue';
+import './style.css';
+import App from './App.vue';
 
-createApp(App).mount('#app')
+createApp(App).mount('#app');
 
 const fun = () => {
-  console.log("sdfsdf")  
-}
-fun()
+  console.log('sdfsdf');
+};
+fun();
