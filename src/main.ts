e516fc3fad2512b136bce5db95c92169f162d139
fzw-code <81961566+fzw-code@users.noreply.github.com>
2023-07-21 16:18:48 +0800

    feat(plugin): 添加element-ui

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,11 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import { createApp } from 'vue';
+import './style.css';
+import App from './App.vue';
 // 引入element-plus插件
-import ElementPlus from 'element-plus'
-import 'element-plus/dist/index.css'
+import ElementPlus from 'element-plus';
+import 'element-plus/dist/index.css';
 
-const app = createApp(App)
+const app = createApp(App);
 
-app.use(ElementPlus)
-app.mount('#app')
+app.use(ElementPlus);
+app.mount('#app');
