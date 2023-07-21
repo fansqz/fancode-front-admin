cbc74dd6a221016892c53e3ea6ddf39006d77041
fzw-code <81961566+fzw-code@users.noreply.github.com>
2023-07-21 21:05:49 +0800

    feat(env): 添加不同的环境变量

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import { createApp } from 'vue';
-import './style.css';
-import App from './App.vue';
+import '@/style.css';
+import App from '@/App.vue';
 // 引入element-plus插件
 import ElementPlus from 'element-plus';
 import 'element-plus/dist/index.css';
