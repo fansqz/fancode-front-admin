f6f375d77876b6763d8ee451b8c28b4c13058141
fzw-code <81961566+fzw-code@users.noreply.github.com>
2023-07-31 01:15:32 +0800

    feat(api): 添加用户管理的api

---
+++ src/api/user/index.ts
@@ -0,0 +1,51 @@
+import request from '@/utils/request';
+import { toFormData } from '@/utils/format';
+
+enum API {
+  //获取用户列表
+  LIST_USER_URL = '/manage/user/list',
+  // 用户相关url
+  USER_URL = '/manage/user',
+  // 用户-角色控制
+  USER_ROLE_URL = '/manage/user/role',
+}
+
+// 获取用户列表
+export const reqUserList = (data: any): Promise<any> => {
+  return request.get(API.LIST_USER_URL, {
+    params: data,
+  });
+};
+
+// 创建用户
+export const reqInsertUser = (data: any): Promise<any> => {
+  return request.post(API.USER_URL, toFormData(data), {
+    headers: {
+      'Content-Type': 'multipart/form-data',
+    },
+  });
+};
+
+// 修改用户
+export const reqUpdateUser = (data: any): Promise<any> => {
+  return request.put(API.USER_URL, toFormData(data), {
+    headers: {
+      'Content-Type': 'multipart/form-data',
+    },
+  });
+};
+
+// 删除角色
+export const reqDeleteUser = (id: number): Promise<any> => {
+  return request.delete(API.USER_URL + '/' + id);
+};
+
+// 获取用户的角色id
+export const reqUserApi = (userID: number): Promise<any> => {
+  return request.get(API.USER_ROLE_URL + `/${userID}`);
+};
+
+// 更新角色可访问api
+export const reqUpdateUserApi = (data: any): Promise<any> => {
+  return request.put(API.USER_ROLE_URL, data);
+};
