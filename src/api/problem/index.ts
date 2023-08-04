b7b71b1c2f53ccc23ba3835d3d03e8be27ec4425
fzw-code <81961566+fzw-code@users.noreply.github.com>
2023-08-04 22:01:09 +0800

    style(url): 修改一下请求路径

--- src/api/problem/index.ts
+++ src/api/problem/index.ts
@@ -2,16 +2,10 @@ import request from '@/utils/request';
 import { toFormData } from '@/utils/format';
 
 enum API {
+  //题目相关url
+  PROBLEM_URL = 'manage/problem',
   //获取题目列表
-  LIST_PROBLEM_URL = '/problem/list/',
-  //创建题目
-  CREATE_PROBLEM_URL = '/problem/insert',
-  //获取题目
-  GET_PROBLEM_URL = '/problem/get',
-  // 更新题目
-  UPDATE_PROBLEM_URL = '/problem/update',
-  // 删除题目
-  DELETE_PROBLEM_URL = '/problem/delete',
+  LIST_PROBLEM_URL = 'manage/problem/list/',
   // 下载题目编程文件
   DOWNLOAD_PROBLEM_FILE_URL = '/problem/file/download',
   // 下载题目编程文件的模板文件
@@ -27,17 +21,17 @@ export const reqProblemList = (page: number, limit: number): Promise<any> => {
 
 // 创建题目
 export const reqCreateProblem = (): Promise<any> => {
-  return request.post<any, any>(API.CREATE_PROBLEM_URL);
+  return request.post<any, any>(API.PROBLEM_URL);
 };
 
 // 根据id获取题目信息
 export const reqGetProblem = (id: string): Promise<any> => {
-  return request.get<any, any>(API.GET_PROBLEM_URL + '/' + id);
+  return request.get<any, any>(API.PROBLEM_URL + '/' + id);
 };
 
 // 修改题目
 export const reqUpdateProblem = (data: any): Promise<any> => {
-  return request.put(API.UPDATE_PROBLEM_URL, toFormData(data), {
+  return request.put(API.PROBLEM_URL, toFormData(data), {
     headers: {
       'Content-Type': 'multipart/form-data',
     },
@@ -46,7 +40,7 @@ export const reqUpdateProblem = (data: any): Promise<any> => {
 
 // 删除题目
 export const reqDeleteProblem = (id: number): Promise<any> => {
-  return request.delete(API.DELETE_PROBLEM_URL + '/' + id);
+  return request.delete(API.PROBLEM_URL + '/' + id);
 };
 
 // 下载题目的编程文件
